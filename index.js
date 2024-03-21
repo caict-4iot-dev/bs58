db82bea6c58e5dfe230c06e21894e6a4671f2bf5
maxufeng <maxufeng@caict.ac.cn>
2024-03-21 10:19:39 +0800

    [feat] fit bifchain

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const basex = require('base-x')
-const ALPHABET = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
+const ALPHABET = '123456789AbCDEFGHJKLMNPQRSTuVWXYZaBcdefghijkmnopqrstUvwxyz'
 
 module.exports = basex(ALPHABET)
